e3b0664dcf594faae312bd1632859063e4572d2e
BelacEr <caleblopezmorgado2007@gmail.com>
2025-07-15 13:25:53 -0600

    automate with python

--- 05_Dictionaries_And_Structuring_Data/Practice_questions/chessDictionaryValidator.py
+++ 05_Dictionaries_And_Structuring_Data/Practice_questions/chessDictionaryValidator.py
@@ -4,30 +4,30 @@ def isValidChessBoard(board):
 
 
 
-def get_chess_board_from_user():
-    board = {}
-    print("Enter chess pieces in format 'position:piece' (e.g., '1a:wking')")
-    print("Enter 'done' when finished:")
+    def get_chess_board_from_user():
+        board = {}
+        print("Enter chess pieces in format 'position:piece' (e.g., '1a:wking')")
+        print("Enter 'done' when finished:")
     
-    while True:
-        entry = input("> ").strip().lower()
-        if entry == 'done':
-            break
+        while True:
+            entry = input("> ").strip().lower()
+            if entry == 'done':
+                break
         
-        try:
-            pos, piece = entry.split(':')
-            board[pos] = piece
-        except ValueError:
-            print("Invalid format! Please use 'position:piece'")
+            try:
+                pos, piece = entry.split(':')
+                board[pos] = piece
+            except ValueError:
+                print("Invalid format! Please use 'position:piece'")
     
-    return board
+        return board
 
-# Main program
-if __name__ == "__main__":
-    print("CHESS BOARD VALIDATOR")
+    # Main program
+    if __name__ == "__main__":
+       print("CHESS BOARD VALIDATOR")
     chess_board = get_chess_board_from_user()
     
     if isValidChessBoard(chess_board):
-        print("\n Valid chess board!")
+            print("\n Valid chess board!")
     else:
-        print("\n Invalid chess board!")
+            print("\n Invalid chess board!")
