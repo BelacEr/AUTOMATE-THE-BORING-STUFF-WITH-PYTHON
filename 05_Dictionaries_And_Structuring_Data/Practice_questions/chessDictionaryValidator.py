251e7d3954676ba2975931551014bc34832b041d
BelacEr <caleblopezmorgado2007@gmail.com>
2025-07-14 20:23:34 -0600

    automate with python

---
+++ 05_Dictionaries_And_Structuring_Data/Practice_questions/chessDictionaryValidator.py
@@ -0,0 +1,33 @@
+def isValidChessBoard(board):
+    # (Keep all the validation logic from the previous implementation)
+     # ... [previous validation code here] ...
+
+
+
+def get_chess_board_from_user():
+    board = {}
+    print("Enter chess pieces in format 'position:piece' (e.g., '1a:wking')")
+    print("Enter 'done' when finished:")
+    
+    while True:
+        entry = input("> ").strip().lower()
+        if entry == 'done':
+            break
+        
+        try:
+            pos, piece = entry.split(':')
+            board[pos] = piece
+        except ValueError:
+            print("Invalid format! Please use 'position:piece'")
+    
+    return board
+
+# Main program
+if __name__ == "__main__":
+    print("CHESS BOARD VALIDATOR")
+    chess_board = get_chess_board_from_user()
+    
+    if isValidChessBoard(chess_board):
+        print("\n Valid chess board!")
+    else:
+        print("\n Invalid chess board!")
